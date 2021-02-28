ba6405ce4091f89cc4cd385fd994027c639f1bea
yoyo-a11 <yoyol264@deltalearns.ca>
2021-02-27 19:14:01 -0800

    005b

--- task4.py
+++ task4.py
@@ -7,21 +7,6 @@ Return False if the number is not an integer
 (2 points)
 """
 
-def is_integer_num(n):
-    if isinstance(n, int):
-        return True
-    if isinstance(n, float):
-        return n.is_integer()
-    return False
-
-print(is_integer_num(100))
-# True
-
-print(is_integer_num(1.23))
-# False
-
-print(is_integer_num(100.0))
-# True
-
-print(is_integer_num('100'))
-# False
+def isInteger(n):
+    return isinstance(n, int)
+    
